111b8b442a9a153e9c9cfc3e3f432cb814f096d0
Elias Kuiter <info@elias-kuiter.de>
2014-04-14 10:22:14 +0200

    Disqus added
    FrameBorder fix

--- index.php
+++ index.php
@@ -291,6 +291,15 @@ code;
     echo <<<code
 		</div>
     </div>
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+      var disqus_shortname = 'eliaskuiter'; // required: replace example with your forum shortname
+      (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    </script>
 	<div class="row margin-top" style="text-align:right;color:#aaa">
 	<p style="line-height:0.8em">
   <a href="http://www.w3.org/html/logo/">
