f17c2260cfc09599b09e6a47532b34f21f899b0b
Elias Kuiter <info@elias-kuiter.de>
2014-05-29 10:17:46 +0200

    Configuration template

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
-$GLOBALS['update_interval'] = 60;
-$GLOBALS['github_username'] = 'ekuiter';
-$GLOBALS['github_password'] = '***REMOVED***';
-$GLOBALS['youtube_username'] = 'ekuiter';
-$GLOBALS['googleplay_apps'] = array('ek.ebersuche', 'ek.hcp.templog', 'ek.waeco.templog');
-
 if (strstr($_SERVER['HTTP_HOST'], 'localhost')) {
   error_reporting(-1);
   ini_set('display_errors', 1); 
@@ -13,8 +7,9 @@ if (strstr($_SERVER['HTTP_HOST'], 'localhost')) {
   error_reporting(0);
   ini_set('display_errors', 0);
 }
-date_default_timezone_set('Europe/Berlin');#
+date_default_timezone_set('Europe/Berlin');
 
+require_once 'include/config.inc.php';
 require_once 'include/Feed.class.php';
 require_once 'include/FolderGrid.class.php';
 require_once 'include/LatestContent.class.php';
